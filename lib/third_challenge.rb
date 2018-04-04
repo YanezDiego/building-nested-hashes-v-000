145f4a6895d7705844f1f2181cfc93d18fadf652
Diego Yanez <diegosyanez92@gmail.com>
2018-04-04 14:20:56 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -28,7 +28,7 @@ def third_challenge
       :heroine => {
         name: "Juliet",
         age: "15",
-        status: "slive"
+        status: "alive"
         },
       :heroine_friends => []
    }
