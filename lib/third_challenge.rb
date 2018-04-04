7a95e02e121c06b6f53172e8a40bb257780d0b82
Diego Yanez <diegosyanez92@gmail.com>
2018-04-04 14:16:43 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,9 +10,21 @@ def third_challenge
         age: "54"
         },
       :hero => {
-        #your key/value pairs here
+        name: "Romeo",
+        age: "15",
+        status: "Alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {
+         name: "Benvolio",
+         age: "17",
+         attitude: "worried"
+       },
+       {
+         name: "Mercutio",
+         age: "18",
+         attitude: "hot-headed"
+      ]
    },
    :capulet => {
       :patriarch => {
