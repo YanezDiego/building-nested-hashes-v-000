42ba0bd8153cf24cd28287798a729f80ae82ce7a
Diego Yanez <diegosyanez92@gmail.com>
2018-04-04 14:19:53 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,33 +1,5 @@
 def first_challenge
    epic_tragedy = {
      montagues:{}
-       hero_friends:[
-          {
-           name: "Benvolio",
-           age: "17",
-           attitude: "worried"
-         },
-         {
-           name: "Mercutio",
-           age: "18",
-           attitude: "hot-headed"
-         }
-       ]
-     },
      capulets:{}
-       heroine_friends:[
-         {
-           name: "Steven",
-           age: "30",
-           attitude:"Confused"
-         },
-         {
-           name: "Nurse",
-           age: "44",
-           attitude: "Worried"
-         }
-       ]
-     }
-
-   }
 end
