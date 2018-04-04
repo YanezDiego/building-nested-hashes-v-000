1b9005b97a2f57e5d496d251048bc7048613d28a
Diego Yanez <diegosyanez92@gmail.com>
2018-04-04 14:17:55 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,25 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     montagues: {
-       patriarch: {
-         name: "Lord Montague",
-         age: "53"
-       },
-       matriarch: {
-         name: "Lady Montague",
-         age: "54"
-       },
-       hero: {
-         name: "Romeo",
-         age: "15",
-         status: "Alive"
-       },
-       hero_friends:[
-          {
-           name: "Benvolio",
-           age: "17",
-           attitude: "worried"
-         },
+     montagues: {},
          {
            name: "Mercutio",
            age: "18",
