5aaf947aac152f0aeec68d858a0f4148be621cfb
Diego Yanez <diegosyanez92@gmail.com>
2018-04-04 14:21:56 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -23,9 +23,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {
-          name: "Steven",
-          age: "30",
-          attitude:"Confused"
+          name: "Steven", age: "30", attitude:"Confused"
         },
         {
           name: "Nurse",
